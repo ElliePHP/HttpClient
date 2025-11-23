e0a340a2d7235d7cd673fc76102c72363cf587e5
Joey 😎 <43940776+joeyboli@users.noreply.github.com>
2025-11-23 16:29:53 +0000

    Update Http.php

--- src/Http.php
+++ src/Http.php
@@ -16,6 +16,28 @@ namespace ElliePHP\Components\HttpClient;
  * $response = Http::withBaseUrl('https://api.example.com')->get('/users');
  * $response = Http::withToken('token')->get('/api/protected');
  * ```
+ * 
+ * @method static Response get(string $url, array $query = []) Send a GET request
+ * @method static Response post(string $url, array $data = []) Send a POST request
+ * @method static Response put(string $url, array $data = []) Send a PUT request
+ * @method static Response patch(string $url, array $data = []) Send a PATCH request
+ * @method static Response delete(string $url) Send a DELETE request
+ * @method static ClientBuilder withBaseUrl(string $baseUrl) Set the base URL for requests
+ * @method static ClientBuilder withHeaders(array $headers) Add custom headers to the request
+ * @method static ClientBuilder withHeader(string $name, string $value) Set a single header
+ * @method static ClientBuilder withUserAgent(string $userAgent) Set the User-Agent header
+ * @method static ClientBuilder withContentType(string $contentType) Set the Content-Type header
+ * @method static ClientBuilder withAccept(string $accept) Set the Accept header
+ * @method static ClientBuilder withToken(string $token) Add Bearer token authentication
+ * @method static ClientBuilder withBasicAuth(string $username, string $password) Add Basic authentication
+ * @method static ClientBuilder acceptJson() Set Accept header to application/json
+ * @method static ClientBuilder asJson() Set Content-Type header to application/json and enable JSON encoding
+ * @method static ClientBuilder withTimeout(int $seconds) Set request timeout in seconds
+ * @method static ClientBuilder withMaxRedirects(int $maxRedirects) Set maximum number of redirects to follow
+ * @method static ClientBuilder withVerify(bool $verify = true) Enable or disable SSL certificate verification
+ * @method static ClientBuilder withProxy(string $proxyUrl) Configure proxy settings
+ * @method static ClientBuilder withRetry(array $retryConfig) Configure retry behavior for failed requests
+ * @method static ClientBuilder withOptions(array $options) Set additional Symfony HttpClient options
  */
 class Http
 {
